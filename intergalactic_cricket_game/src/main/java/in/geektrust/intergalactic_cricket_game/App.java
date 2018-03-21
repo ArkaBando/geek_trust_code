b69afa7c59fcf72da27027adf1475b41f8d7f251
arkabandyopadhyay <arkabandyopadhyay2015@gmail.com>
2018-03-21 10:32:12 +0530

    init commit

---
+++ intergalactic_cricket_game/src/main/java/in/geektrust/intergalactic_cricket_game/App.java
@@ -0,0 +1,14 @@
+package in.geektrust.intergalactic_cricket_game;
+
+/**
+ * Main Driver Class
+ * @author arka
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        
+    }
+}
---
+++ intergalactic_cricket_game/src/main/java/in/geektrust/intergalactic_cricket_game/util/Radomizer.java
@@ -0,0 +1,6 @@
+package in.geektrust.intergalactic_cricket_game.util;
+
+public interface Radomizer {
+
+	public Integer randomize();
+}
