f4f32ba799b996394e7c9c43300e0436504f34ed
arkabandyopadhyay <arkabandyopadhyay2015@gmail.com>
2018-03-23 00:55:49 +0530

    refrator done

--- lengaburu-traffic/src/main/java/in/geektrust/lengaburu/traffic/context/ProblemContext.java
+++ lengaburu-traffic/src/main/java/in/geektrust/lengaburu/traffic/context/ProblemContext.java
@@ -40,35 +40,40 @@ public class ProblemContext implements AppContext{
 			System.out.printf(outputMessage, optimalVehicle.getVechileType(), optimalOrbit[0].getOrbitName());
 			break;
 		case "Problem2" :
-			Orbit sourceOrbit = optimalOrbit[0];
-			Orbit destinationOrbit = optimalOrbit[1];
-			
-			//------------------------------ Orbit rearranged if required
-			if(destinationOrbit.getOrbitStartingPoint().equalsIgnoreCase(source)){
-				sourceOrbit = optimalOrbit[1];
-				destinationOrbit = optimalOrbit[0];
-			}
-			
-			if(destinationOrbit.getOrbitEndingPoint().equalsIgnoreCase(source)){
-				sourceOrbit = optimalOrbit[1];
-				String src = optimalOrbit[1].getOrbitStartingPoint();
-				sourceOrbit.setOrbitEndingPoint(src);
-				sourceOrbit.setOrbitStartingPoint(source);
-				destinationOrbit = optimalOrbit[0];
-			}
-			//---------------------------------
-			System.out.printf(outputMessage,
-					optimalVehicle.getVechileType(),
-					sourceOrbit.getOrbitEndingPoint(),
-					sourceOrbit.getOrbitName(),
-					destinationOrbit.getOrbitEndingPoint().equalsIgnoreCase(sourceOrbit.getOrbitEndingPoint())?
-					destinationOrbit.getOrbitStartingPoint():destinationOrbit.getOrbitEndingPoint()	,
-					destinationOrbit.getOrbitName());
+			processProblem2Output(source, optimalOrbit, optimalVehicle);
 			
 			break;
 		default:
 			break;
 		}
+	}
+
+	private void processProblem2Output(String source, Orbit[] optimalOrbit,
+			Vehicle optimalVehicle) {
+		Orbit sourceOrbit = optimalOrbit[0];
+		Orbit destinationOrbit = optimalOrbit[1];
+		
+		//------------------------------ Orbit rearranged if required
+		if(destinationOrbit.getOrbitStartingPoint().equalsIgnoreCase(source)){
+			sourceOrbit = optimalOrbit[1];
+			destinationOrbit = optimalOrbit[0];
+		}
+		
+		if(destinationOrbit.getOrbitEndingPoint().equalsIgnoreCase(source)){
+			sourceOrbit = optimalOrbit[1];
+			String src = optimalOrbit[1].getOrbitStartingPoint();
+			sourceOrbit.setOrbitEndingPoint(src);
+			sourceOrbit.setOrbitStartingPoint(source);
+			destinationOrbit = optimalOrbit[0];
+		}
+		//---------------------------------
+		System.out.printf(outputMessage,
+				optimalVehicle.getVechileType(),
+				sourceOrbit.getOrbitEndingPoint(),
+				sourceOrbit.getOrbitName(),
+				destinationOrbit.getOrbitEndingPoint().equalsIgnoreCase(sourceOrbit.getOrbitEndingPoint())?
+				destinationOrbit.getOrbitStartingPoint():destinationOrbit.getOrbitEndingPoint()	,
+				destinationOrbit.getOrbitName());
 	}
 	
 	public void selectOutputMessage(){
